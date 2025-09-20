6a7cd0a72e87a6379caa5e1d789736a95d73aa6c
ZenoVaScript <wildangtwildangt@gmail.com>
2025-09-21 02:54:39 +0700

    Add files via upload

--- Zenofield/Zenofield-Ui.lua
+++ Zenofield/Zenofield-Ui.lua
@@ -117,7 +117,7 @@ local useStudio = RunService:IsStudio() or false
 local settingsCreated = false
 local settingsInitialized = false -- Whether the UI elements in the settings page have been set to the proper values
 local cachedSettings
-local prompt = useStudio and require(script.Parent.prompt) or loadWithTimeout('https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/prompt.lua')
+local prompt = useStudio and require(script.Parent.prompt) or loadWithTimeout('https://raw.githubusercontent.com/ZenoVaScript/Zeno-Ui/main/Zenofield/Zprompt.lua')
 local requestFunc = (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request) or http_request or request
 
 -- Validate prompt loaded correctly
@@ -742,7 +742,7 @@ Zenofield.DisplayOrder = 100
 LoadingFrame.Version.Text = Release
 
 -- Thanks to Latte Softworks for the Lucide integration for Roblox
-local Icons = useStudio and require(script.Parent.icons) or loadWithTimeout('https://raw.githubusercontent.com/SiriusSoftwareLtd/Zenofield/refs/heads/main/icons.lua')
+local Icons = useStudio and require(script.Parent.icons) or loadWithTimeout('https://raw.githubusercontent.com/ZenoVaScript/Zeno-Ui/main/Zenofield/icons.lua')
 -- Variables
 
 local CFileName = nil
@@ -3981,7 +3981,7 @@ if CEnabled and Main:FindFirstChild('Notice') then
 end
 -- AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA why :(
 --if not useStudio then
---	task.spawn(loadWithTimeout, "https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/boost.lua")
+--	task.spawn(loadWithTimeout, "https://raw.githubusercontent.com/ZenoVaScript/Zeno-Ui/main/Zenofield/Zboost.lua")
 --end
 
 task.delay(4, function()
